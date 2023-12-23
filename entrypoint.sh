159cb6ced8bc4830135c5dd2060110cc22c20a7c
dftxhegfgfdhadc <105432865+dftxhegfgfdhadc@users.noreply.github.com>
2023-12-23 10:31:01 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_Path="/45eea729-91cc-4310-8072-31e3cb93252f"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_UUID="45eea729-91cc-4310-8072-31e3cb93252f"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_Path="/2a8f5bbf-fe67-4e3d-9d06-ebd088cce9f3"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_UUID="2a8f5bbf-fe67-4e3d-9d06-ebd088cce9f3"
 fi
 echo ${Vmess_UUID}
 
